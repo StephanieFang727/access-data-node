3d2593a3dae96747cead012b55032fffb5cb0da7
StephanieFang727 <fangjie_727@163.com>
2024-09-04 18:06:16 +0800

    feat: chatbot

--- src/controller/chatbotController.ts
+++ src/controller/chatbotController.ts
@@ -1,18 +1,53 @@
-import { Context } from "koa";
-import chatbotModel from "../model/chatbotModel";
 import { z } from "zod";
+import { RunnableSequence } from "@langchain/core/runnables";
+import { StructuredOutputParser } from "@langchain/core/output_parsers";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import chatbotModel from "../model/chatbotModel";
+import { JsonOutputParser } from "@langchain/core/output_parsers";
+import { StringOutputParser } from "@langchain/core/output_parsers";
+
+const TypeEnum = z.enum([
+  "@fangjiefe/line-chart",
+  "@fangjiefe/pie-chart",
+  "@fangjiefe/bar-chart",
+]);
+
+const attributesSchema = z.object({
+  title: z.string().optional().describe("图表标题"),
+  xAxisTitle: z.string().optional().default("").describe("图表x轴标题"),
+  yAxisTitle: z.string().optional().default("").describe("图表y轴标题"),
+  useLegend: z.boolean().optional().default(true).describe("是否使用图例"),
+  useTooltip: z.boolean().optional().default(true).describe("是否使用提示"),
+});
+
+const stylesSchema = z.object({
+  titleFontSize: z.number().optional().default(16).describe("标题字体大小"),
+});
+
+const cptSchema = z.object({
+  // id: z.string().describe("answer to the user's question"),
+  type: TypeEnum.describe("图表类型对应的组件库名"),
+  props: z.object({
+    attributes: attributesSchema,
+    styles: stylesSchema,
+  }),
+});
 
 export const chat = async (question: string) => {
-  const joke = z.object({
-    setup: z.string().describe("The setup of the joke"),
-    punchline: z.string().describe("The punchline to the joke"),
-    rating: z
-      .number()
-      .optional()
-      .describe("How funny the joke is, from 1 to 10"),
+  const parser = StructuredOutputParser.fromZodSchema(cptSchema);
+  const chain = RunnableSequence.from([
+    ChatPromptTemplate.fromTemplate(
+      "请回答用户的问题\n{format_instructions}\n{question}"
+    ),
+    chatbotModel,
+    parser,
+  ]);
+  // console.log(parser.getFormatInstructions());
+  const response = await chain.invoke({
+    question,
+    format_instructions: parser.getFormatInstructions(),
   });
 
-  const structuredLlm = chatbotModel.withStructuredOutput(joke);
-  console.log("chatbotModel", await chatbotModel.invoke(question));
-  return await structuredLlm.invoke(question);
+  console.log(response);
+  return response;
 };
